a46352b0ca66db762db51edc3377925fdb25fc27
Coffee-crocodile <1147347984@qq.com>
2023-06-30 11:36:11 +0800

    fix(build): remove plugins

--- build/plugins/vue.ts
+++
@@ -1,6 +0,0 @@
-import vue from '@vitejs/plugin-vue';
-import vueJsx from '@vitejs/plugin-vue-jsx'; // https://github.com/vitejs/vite/tree/main/packages/plugin-vue-jsx
-
-const plugins = [vue(), vueJsx()];
-
-export default plugins;
